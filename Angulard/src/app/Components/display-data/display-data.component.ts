6e50dc27e3a3a7913b32616c13a877d1e4547030
Saulix187203 <146505863+ElFeral@users.noreply.github.com>
2025-03-30 17:32:30 -0600

    06/03/2025 - paso16: crear un nuevo componente llamado display-data

---
+++ Angulard/src/app/Components/display-data/display-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-data',
+  imports: [],
+  templateUrl: './display-data.component.html',
+  styleUrl: './display-data.component.css'
+})
+export class DisplayDataComponent {
+
+}
