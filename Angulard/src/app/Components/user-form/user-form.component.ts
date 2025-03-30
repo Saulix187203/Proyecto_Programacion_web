b5c0240c54b9ff2e9fa3e048671a6e17831d8a0b
Saulix187203 <146505863+ElFeral@users.noreply.github.com>
2025-03-30 13:50:50 -0600

    06/03/2025 - paso2,3,4:importar dependencias y modificar UserFormComponent

--- Angulard/src/app/Components/user-form/user-form.component.ts
+++ Angulard/src/app/Components/user-form/user-form.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    EventEmitter,
+    Output,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
-
+  nombre: string = "";
+  edad: number | null = null;
+  @Output() userSumbit = new EventEmitter<{ nombre: string; edad: number }>();
+  subirForm() {
+    if (this.nombre && this.edad !== null) {
+      this.userSumbit.emit({nombre: this.nombre, edad: this.edad});
+      this.nombre = "";
+      this.edad = null;
+    }
+  }
 }
