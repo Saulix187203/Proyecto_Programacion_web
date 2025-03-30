fd7b6d76387cf8faf3a7323b5d421b4afca762fc
Saulix187203 <146505863+ElFeral@users.noreply.github.com>
2025-03-30 17:13:36 -0600

    06/03/2025 - paso13: modificar el archivo user-form-primeflex.components.ts

--- Angulard/src/app/Components/user-form-primeflex/user-form-primeflex.component.ts
+++ Angulard/src/app/Components/user-form-primeflex/user-form-primeflex.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-user-form-primeflex',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCardModule
+  ],
   templateUrl: './user-form-primeflex.component.html',
   styleUrl: './user-form-primeflex.component.css'
 })
 export class UserFormPrimeflexComponent {
+  name: string = '';
+  age: number | null = null;
 
+  @Output() userSumbit = new EventEmitter<{ name: string; age: number }>();
+  subirForm() {
+    if (this.name && this.age !== null) {
+      this.userSumbit.emit({ name: this.name, age: this.age });
+      this.name = "";
+      this.age = null;
+    }
+  }
 }
