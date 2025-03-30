b403e8704cf66edaf9c6b53cc1fa71b3bb108dd2
Saulix187203 <146505863+ElFeral@users.noreply.github.com>
2025-03-30 17:41:55 -0600

    06/03/2025 - paso18: importar los modulos necesarios y modificar el export class para recibir datos

--- Angulard/src/app/Components/display-data/display-data.component.ts
+++ Angulard/src/app/Components/display-data/display-data.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-display-data',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './display-data.component.html',
   styleUrl: './display-data.component.css'
 })
 export class DisplayDataComponent {
+  @Input() name: string = '';
+  @Input() age: number | null = null;
 
 }
