8ee412ad2229e29dbddb19217e953ab1e228bbd2
Saulix187203 <146505863+ElFeral@users.noreply.github.com>
2025-03-30 16:21:16 -0600

    06/03/2025 - paso7: importar componetes al component principal de Angular

--- Angulard/src/app/app.component.ts
+++ Angulard/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrincipalComponent } from './Components/principal/principal.component';
+import { UserFormComponent } from './Components/user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PrincipalComponent],
+  imports: [RouterOutlet, PrincipalComponent, UserFormComponent, CommonModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
